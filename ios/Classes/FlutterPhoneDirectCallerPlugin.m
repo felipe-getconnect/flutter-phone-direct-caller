32c1e1ce0cc76c3504548832ef46d74aa0f918f3
Felipe Ferreira <felipe.ferreira@getconnect.com.br>
2024-11-04 13:37:54 -0300

    fix: Replace a removed API call in IOS 18

--- ios/Classes/FlutterPhoneDirectCallerPlugin.m
+++ ios/Classes/FlutterPhoneDirectCallerPlugin.m
@@ -2,37 +2,43 @@
 
 @implementation FlutterPhoneDirectCallerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_phone_direct_caller"
-            binaryMessenger:[registrar messenger]];
-  FlutterPhoneDirectCallerPlugin* instance = [[FlutterPhoneDirectCallerPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+            methodChannelWithName:@"flutter_phone_direct_caller"
+                  binaryMessenger:[registrar messenger]];
+    FlutterPhoneDirectCallerPlugin* instance = [[FlutterPhoneDirectCallerPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else if ([@"callNumber" isEqualToString:call.method]) {
-      NSString* number = call.arguments[@"number"];
-      result(@([self directCall:number]));
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([@"callNumber" isEqualToString:call.method]) {
+        NSString* number = call.arguments[@"number"];
+        result(@([self directCall:number]));
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 - (BOOL)directCall:(NSString*)number {
     number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    if( ! [number hasPrefix:@"tel:"]){
-        number =  [NSString stringWithFormat:@"tel:%@", number];
+    if (![number hasPrefix:@"tel:"]) {
+        number = [NSString stringWithFormat:@"tel:%@", number];
     }
-    if(![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:number]]) {
+
+    NSURL *url = [NSURL URLWithString:number];
+    if (![[UIApplication sharedApplication] canOpenURL:url]) {
         return NO;
-    } else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
-        // missing phone number
-        return NO;
-    } else {
-        return YES;
     }
+
+    // Use the new `openURL` method
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
+        if (!success) {
+            NSLog(@"Failed to open URL: %@", number);
+        }
+    }];
+
+    return YES;
 }
 
 @end
